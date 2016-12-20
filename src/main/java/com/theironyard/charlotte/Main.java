56f7dde7d9f9f8cc76abe47e3028089363ffe635
Stephen Williamson <stephenwilliamson@Stephens-MacBook.local>
2016-12-20 14:47:16 -0500

    Added homepage

--- src/main/java/com/theironyard/charlotte/Main.java
+++ src/main/java/com/theironyard/charlotte/Main.java
@@ -1,8 +1,15 @@
 package com.theironyard.charlotte;
 
+import spark.Spark;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Spark.get("/", (((request, response) -> "Hello, World!"));
+
+        Spark.before((request, response) -> {
+            response.header("Access-Control-Allow-Origin", "*");
+        });
+
     }
 }
